35900a36e39aaf8361f2a35766944abfc7e4bbcf
今関翔太 <imazekishouta@imazekishoutanoMacBook-Pro.local>
2020-12-04 13:23:58 +0900

    delete blog function

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,9 +3,3 @@ import os
 
 class Config:
     SECRET_KEY = 'cfb33786023cc152019e747a051f73c6'  # os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'  # os.environ.get('SQLALCHEMY_DATABASE_URI')
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')  # 設定しなきゃ
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,4 @@
-from flask import render_template, request, Blueprint
-from flaskblog.models import Post
+from flask import render_template, Blueprint
 import markdown
 
 
@@ -9,14 +8,6 @@ main = Blueprint('main', __name__)
 @main.route('/')
 @main.route('/home')
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(
-        Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
-
-
-@main.route('/about')
-def about():
     readme_file = open("flaskblog/markdown/ABOUT.md", "r")
     md_to_html = markdown.markdown(readme_file.read(), encoding="utf8")
 
